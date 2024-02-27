86ee69cf17c490db9524f93adeb9996302b56c24
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-02-26 22:30:07 -0800

    get request for all users

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/", async (req, res) => {
+app.post("/user", async (req, res) => {
     try {
-        if (!req.body.email || !req.body.password || req.body.name) {
+        if (!req.body.email || !req.body.password || !req.body.name) {
             return res.status(400).send({
             message: 'Send all required fields: email, password, name'
         })
@@ -22,16 +22,27 @@ app.post("/", async (req, res) => {
     };
         const user = await User.create(newUser);
         return res.status(201).send(user);
-    } catch {
+    } catch (error) {
         console.log(error.message);
         res.status(500).send({ messsge: error.message })
     }
-
 }) 
+
+app.get("/user", async (req, res) => {
+    try {
+        const users = await User.find({});
+
+        return res.status(200).json(users);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message})
+    }
+})
 
 app.get("/", (req, res) => {
     console.log(req);
-    res.send("I Love Gloria");
+    res.status(223).send("I Love Gloria");
 })
 
 mongoose
