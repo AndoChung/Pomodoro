8ca7942c1860d121db70d1273ed8032483d7cc20
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-02-28 00:10:01 -0800

    added delete single user request

--- backend/server.js
+++ backend/server.js
@@ -83,6 +83,22 @@ app.put("/user/:id", async (req, res) => {
     }
 })
 
+app.delete("/user/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await User.findByIdAndDelete(id);
+
+        if (!result) {
+            return res.status(404).json({ message: "Book not found"});
+        }
+
+        return res.status(200).send({ message: "Book deleted successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+})
 
 app.get("/", (req, res) => {
     res.status(223).send("I Love Gloria");
