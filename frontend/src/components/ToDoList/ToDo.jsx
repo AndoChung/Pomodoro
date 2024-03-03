82e0e99efe26feb4cf6cda7d15397fb3db99c9b2
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-03-02 23:31:22 -0800

    styled todolist component

--- frontend/src/components/ToDoList/ToDo.jsx
+++ frontend/src/components/ToDoList/ToDo.jsx
@@ -4,7 +4,7 @@ const ToDo = ({ goal }) => {
     
   return (
     <div>
-        <div className='flex border-2 m-2 p-2'>
+        <div className='flex border-amber-900 border m-2 p-2'>
             <div className='flex flex-col justify-center pr-4 pl-2'>
                 <input type='checkbox' className='rounded-full px-3' />
             </div>
--- frontend/src/components/ToDoList/ToDoList.jsx
+++ frontend/src/components/ToDoList/ToDoList.jsx
@@ -4,11 +4,15 @@ import ToDo from './ToDo'
 
 const ToDoList = () => {
   return (
-    <div className='w-1/3 overflow-y-scroll'>
+    <div className='w-1/3 overflow-y-scroll border border-gray-800 m-2'>
       <div className=''>
         <ToDo goal="Finish LeetCode"/>
         <ToDo goal="Kiss Gloria and watch her do awesome things from the side and more loveydovey stuff" />
+        <ToDo goal="Have fun with glo and then force her to go rock climbing with me until i show off a move and break a bone so that we use my high deductible kaiser insurance and fall into mad debt so that i have to pick up another jobs and i cant get a software job because of it" />
         <ToDo goal="Kiss Gloria and watch her do awesome things from the side and more loveydovey stuff" />
+        <ToDo goal="Kiss Gloria and watch her do awesome things from the side and more loveydovey stuff" />
+        <ToDo goal="Kiss Gloria and watch her do awesome things from the side and more loveydovey stuff" />
+        <ToDo goal="Kiss Gloria" />
         <ToDo goal="Kiss Gloria and watch her do awesome things from the side and more loveydovey stuff" />
         <ToDo goal="Kiss Gloria and watch her do awesome things from the side and more loveydovey stuff" />
         <ToDo goal="Kiss Gloria and watch her do awesome things from the side and more loveydovey stuff" />
