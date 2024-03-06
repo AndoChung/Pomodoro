522e2e57ef74f3ddb5c24927320434cf6c99fa3e
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-03-06 10:00:12 -0800

    Styled Login Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App.jsx'
+import Home from './pages/Home'
+import Login from  './pages/Login'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <Home />
   },
   {
     path: "/login",
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='h-screen'>
+      <div className='flex justify-center h-5/6'>
+        <form className='flex flex-col justify-center items-center w-1/4'>
+          <div className='h-1/2 border flex flex-col justify-center items-center w-full'>
+            <label htmlFor="username">Username</label>
+            <input type='text' className='w-5/6' />
+            <label htmlFor="password">Password</label>
+            <input type='text' className='w-5/6' />
+            <input type='submit' value="Submit" className='border-gray-900 mt-2' />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
