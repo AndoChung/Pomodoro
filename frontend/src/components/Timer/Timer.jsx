ebfa1e236c5d1519f62b6172e272135656ac31b1
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-03-05 19:45:32 -0800

    Initialized the react-router-dom

--- frontend/src/components/Timer/Timer.jsx
+++ frontend/src/components/Timer/Timer.jsx
@@ -6,11 +6,13 @@ const Timer = ({ amountOfTime }) => {
     const durationNum = Number(amountOfTime)
     let duration = moment.duration(durationNum, "minutes")
     const [display, setDisplay] = useState(moment.utc(duration.as("milliseconds")).format("HH:mm:ss"))
-
     useEffect(() => {
         let interval = setInterval(() => {
             duration.subtract(1000, "milliseconds");
             setDisplay(moment.utc(duration.as("milliseconds")).format("HH:mm:ss"));
+            if (duration.asMilliseconds() === 0) {
+                clearInterval(interval);
+            }
         }, 1000)
         return () => {
             clearInterval(interval);
@@ -18,8 +20,8 @@ const Timer = ({ amountOfTime }) => {
     }, [])
 
     return (
-        <div>
-            <div>{display}</div>
+        <div className='w-2/3 mr-2 mt-2 mb-2 border flex justify-center items-center'>
+            <div className="text-9xl">{display}</div>
         </div>
     )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,8 @@
 import { React, useState, useEffect } from 'react'
 import ToDoList from '../components/ToDoList/ToDoList'
 import Timer from '../components/Timer/Timer'
-import axios from 'axios';
 
 const Home = () => {
-  const [users, setUsers] = useState({});
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/user/')
-      .then((response) => {
-        setUsers(response.data);
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [])
   return (
     <div className='h-screen'>
       <div className='flex h-5/6'>
