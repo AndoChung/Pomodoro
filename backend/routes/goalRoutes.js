392f4a3eb1d198a0362cd30805e07cf544d4b78e
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-03-13 10:15:15 -0700

    added update goal route

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,13 +6,13 @@ const router = express.Router();
 
 
 // Create a Goal
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
     try {
-        if (!req.body.author || !req.body.task) {
-            return res.status(400).send({ message: "Send task and author" })
+        if (!req.body.task) {
+            return res.status(400).send({ message: "Send task" })
         }
         const newGoal = {
-            author: req.body.author,
+            author: req.user,
             task: req.body.task,
         }
         const goal = await Goal.create(newGoal);
@@ -27,12 +27,36 @@ router.post("/", async (req, res) => {
 // Get all posts made by User
 router.get("/", auth, async (req, res) => {
     try{
-        console.log(req.user)
         const goals = await Goal.find({ author : req.user })
         res.status(201).send(goals)
     } catch (error) {
         console.log(error.message);
-        res.status(500).send({ messsge: error.message })
+        res.status(500).send({ messsge: error.message });
+    }
+})
+
+// Update a single post
+router.put("/:id", auth, async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!req.body.task) {
+            return res.status(400).send({
+                message: "Send task"
+            })
+        }
+
+        const updateGoal = await Goal.findByIdAndUpdate(id, {task: req.body.task});
+
+        if (!updateGoal) {
+            return res.status(404).json({ message: "Book not found"});
+            
+        }
+        console.log(req.body.task)
+        return res.status(400).send({ message: "Goal Updated Successfully"})
+    
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ messsge: error.message });
     }
 })
 
