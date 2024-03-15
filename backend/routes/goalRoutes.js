d0c5777c19bf6d809a072e81ba75f1c10d541724
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-03-15 10:55:03 -0700

    Added Delete Goal ROute

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -24,7 +24,7 @@ router.post("/", auth, async (req, res) => {
     }
 })
 
-// Get all posts made by User
+// Get all goal made by User
 router.get("/", auth, async (req, res) => {
     try{
         const goals = await Goal.find({ author : req.user })
@@ -35,7 +35,7 @@ router.get("/", auth, async (req, res) => {
     }
 })
 
-// Update a single post
+// Update a single goal
 router.put("/:id", auth, async (req, res) => {
     try {
         const { id } = req.params;
@@ -48,16 +48,34 @@ router.put("/:id", auth, async (req, res) => {
         const updateGoal = await Goal.findByIdAndUpdate(id, {task: req.body.task});
 
         if (!updateGoal) {
-            return res.status(404).json({ message: "Book not found"});
+            return res.status(404).json({ message: "Goal not found"});
             
         }
-        console.log(req.body.task)
         return res.status(400).send({ message: "Goal Updated Successfully"})
     
     } catch (error) {
         console.log(error.message);
         res.status(500).send({ messsge: error.message });
     }
+})
+
+// Delete goal
+router.delete("/:id", auth, async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deletedGoal = await Goal.findByIdAndDelete(id);
+
+        if (!deletedGoal) {
+            return res.status(404).json({ message: "Goal not found"})
+        }
+        
+        return res.status(400).send({ message: "Goal Successfully Deleted"})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ messsge: error.message });
+    }
+    
 })
 
 export default router;
