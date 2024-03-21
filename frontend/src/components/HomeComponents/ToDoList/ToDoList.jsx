82d161eb6ea1e432dc1ddbc11c4927b34739f4cb
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-03-21 09:51:18 -0700

    mapped goals to todolist

--- frontend/src/components/HomeComponents/ToDoList/ToDoList.jsx
+++ frontend/src/components/HomeComponents/ToDoList/ToDoList.jsx
@@ -18,28 +18,12 @@ const ToDoList = () => {
     getGoals()
   }, [])
 
-  for (let i_goal in goals) {
-    console.log(goals[i_goal]);
-  }
-
   return (
     <div className='w-1/3 overflow-y-scroll border border-gray-800 m-2'>
       <div className=''>
-
-        <ToDo goal="Finish LeetCode"/>
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Have fun with glo and then force her to go rock climbing with me until i show off a move and break a bone so that we use my high deductible kaiser insurance and fall into mad debt so that i have to pick up another jobs and i cant get a software job because of it" />
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Hug Gloria" />
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Hug Gloria" />
-        <ToDo goal="Hug Gloria and watch her do awesome things from the side and more loveydovey stuff" />
-        <ToDo goal="Hug Gloria" />
-        <ToDo goal="Hug Gloria" />
+        {goals.map((goal,i) => (
+          <ToDo goal={goal.task} />
+        ))}
       </div>
     </div>
   )
