f3743b6a57789f0c770167715fccdf7165ecdbcc
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-03-26 18:40:51 -0700

    Initializing Login Page Styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,41 +3,66 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const navigate = useNavigate();
-  const [ userData, setUserData ] = useState({
-    username: '',
-    password: '',
-  });
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState({
+        username: '',
+        password: '',
+    });
 
-  const loginUser = async (e) => {
-    e.preventDefault();
+    const loginUser = async (e) => {
+        e.preventDefault();
         try {
             await axios.post("/user/login", userData);
             navigate("/");
         } catch (error) {
             console.log(error)
         }
-  }
+    }
 
-  return (
-    <div className='h-screen'>
-      <div className='flex justify-center h-5/6'>
-        <form onSubmit={loginUser} className='flex flex-col justify-center items-center w-1/4'>
-          <div className='h-1/2 border flex flex-col justify-center items-center w-full'>
-            <label>Username</label>
-            <input type='text' value={userData.username} onChange={(e) => {
-              setUserData({ ...userData, username: e.target.value})
-            }} className='w-5/6' />
-            <label>Password</label>
-            <input type='password' value={userData.password} onChange={(e) => {
-              setUserData({ ...userData, password: e.target.value})
-            }} className='w-5/6' />
-            <input type='submit' value="Submit" className='border border-black mt-2' />
-          </div>
-        </form>
-      </div>
-    </div>
-  )
+    const navigateRegister = (e) => {
+        e.preventDefault();
+        navigate("/register")
+    }
+
+    return (
+        <div className='h-screen flex w-screen'>
+            <div className='w-1/2 flex flex-col justify-center'>
+                <div className='flex justify-center m-32 flex-col h-1/2'>
+                    <h1 className='text-5xl h-16'>Login</h1>
+                    <div className='h-14'>
+                        <p>See your progress and track your goals!</p>
+                    </div>
+                    <form onSubmit={loginUser} className='flex flex-col items-center h-64 '>
+                        <div className='flex flex-col justify-between w-full h-full'>
+                            <div>
+                                <label>Username*</label>
+                                <input placeholder='username' type='text' value={userData.username} onChange={(e) => {
+                                    setUserData({ ...userData, username: e.target.value })
+                                }} className='w-full h-12' />
+                            </div>
+                            <div>
+                                <label>Password*</label>
+                                <input placeholder='password' type='password' value={userData.password} onChange={(e) => {
+                                    setUserData({ ...userData, password: e.target.value })
+                                }} className='w-full h-12' />
+                            </div>
+                            <div>
+                                <input type='submit' value="Login" className=' h-12 border border-black mt-2 w-full' />
+                            </div>
+                        </div>
+                    </form>
+                    <div className='mt-4'>
+                        <p>First Time Here? <span><button onClick={navigateRegister}>Register Here</button></span></p>
+                    </div>
+                </div>
+            </div>
+            <div className='w-1/2 bg-red-900 h-full'>
+                <div className=''>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,10 @@
-import { React, useState} from 'react'
+import { React, useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
     const navigate = useNavigate();
-    const [ userData, setUserData ] = useState({
+    const [userData, setUserData] = useState({
         name: "",
         email: "",
         password: "",
@@ -21,31 +21,42 @@ const Register = () => {
         }
     }
     return (
-    <div className='h-screen'>
-      <div className='flex justify-center h-5/6'>
-        <form onSubmit={registerUser} className='flex flex-col justify-center items-center w-1/4'>
-          <div className='h-1/2 border flex flex-col justify-center items-center w-full'>
-            <label>Name</label>
-            <input type='text' value={userData.name} onChange={(e) => {
-                setUserData({ ...userData, name: e.target.value})
-            }} className='w-5/6' />
-            <label>Email</label>
-            <input type='email' value={userData.email} onChange={(e) => {
-                setUserData({ ...userData, email: e.target.value})
-            }} className='w-5/6' />
-            <label>Username</label>
-            <input type='text' value={userData.username} onChange={(e) => {
-                setUserData({ ...userData, username: e.target.value})
-            }} className='w-5/6' />
-            <label>Password</label>
-            <input type='password' value={userData.password} onChange={(e) => {
-                setUserData({ ...userData, password: e.target.value})
-            }} className='w-5/6' />
-            <input type='submit' value="Submit" className='border border-black mt-2' />
-          </div>
-        </form>
-      </div>
-    </div>
+        <div className='h-screen flex w-screen'>
+            <div className='w-1/2 bg-red-900 h-full'>
+                <div className=''>
+
+                </div>
+            </div>
+            <div className='w-1/2 flex flex-col justify-center'>
+                <div className='flex justify-center m-32 flex-col h-1/2'>
+                    <h1 className='text-5xl h-16'>Login</h1>
+                    <div className='h-14'>
+                        <p>See your progress and track your goals!</p>
+                    </div>
+                    <form onSubmit={registerUser} className='flex flex-col justify-center items-center w-1/4'>
+                        <div className='h-1/2 border flex flex-col justify-center items-center w-full'>
+                            <label>Name</label>
+                            <input type='text' value={userData.name} onChange={(e) => {
+                                setUserData({ ...userData, name: e.target.value })
+                            }} className='w-5/6' />
+                            <label>Email</label>
+                            <input type='email' value={userData.email} onChange={(e) => {
+                                setUserData({ ...userData, email: e.target.value })
+                            }} className='w-5/6' />
+                            <label>Username</label>
+                            <input type='text' value={userData.username} onChange={(e) => {
+                                setUserData({ ...userData, username: e.target.value })
+                            }} className='w-5/6' />
+                            <label>Password</label>
+                            <input type='password' value={userData.password} onChange={(e) => {
+                                setUserData({ ...userData, password: e.target.value })
+                            }} className='w-5/6' />
+                            <input type='submit' value="Submit" className='border border-black mt-2' />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
